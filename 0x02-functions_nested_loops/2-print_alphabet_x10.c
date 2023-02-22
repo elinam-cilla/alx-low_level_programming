a3447d53c8afed0b56386acd1a1dd98dcb157217
elinam-cilla <ponyameama@gmail.com>
2023-02-22 03:35:57 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char c,
+	int c = 0;
+
+	while (c <= 9)
+	{
+		for (c ='a'; c <= 'z'; c++);
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	c++;
+	}
+}
