a5a6ac14cde58d4d04e0f64eaf5511583f40daa3
elinam-cilla <ponyameama@gmail.com>
2023-02-23 12:34:05 -0800

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,48 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - print largest prime factor of 612852475143
- * Return: 0 on Success
+ * print_number - prints an integer
+ * @n: random integer
+ * Return: void
  */
-int main(void)
+void print_number(int n)
 {
-	unsigned long num = 612852475143;
-	unsigned long i;
-	unsigned long large_prime;
+	char ld, cr;
+	int r;
+	int ct = 0;
 
-	for (i = 2; i <= num; i++)
+	if (n < 0)
 	{
-		if (num % i == 0)
-		{
-			large_prime = i;
-			num /= large_prime;
-		}
+		_putchar ('-');
+		ld = (char)('0' - (n % 10));
+		n = n / -10;
 	}
-	printf("%lu\n", large_prime);
-	return (0);
+
+	else
+	{
+		ld = (char)((n % 10) + '0');
+		n = n / 10;
+	}
+
+	r = 0;
+	while (n > 0)
+	{
+		r = r * 10 + (n % 10);
+		n = n / 10;
+		ct++;
+	}
+
+	while (r > 0)
+	{
+		cr = (char)((r % 10) + '0');
+		_putchar(cr);
+		r = r / 10;
+		ct--;
+	}
+	while (ct != 0)
+	{
+		_putchar('0');
+		ct--;
+	}
+	_putchar(ld);
 }
